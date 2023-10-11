bcab739e843c52756e9b7ff0bc7dad0dce3a574e
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-11 23:36:16 +0500

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './actionPages/log-in/log-in.component';
+import { RegisterComponent } from './actionPages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LogInComponent},
+  {path:'register', component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
