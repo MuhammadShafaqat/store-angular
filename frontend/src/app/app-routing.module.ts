98212188820b858327547e427786c8cc061652e1
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-11 23:42:19 +0500

    add

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './actionPages/log-in/log-in.component';
 import { RegisterComponent } from './actionPages/register/register.component';
+import { ForgotPasswordComponent } from './actionPages/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './actionPages/reset-password/reset-password.component';
 
 const routes: Routes = [
   {path:'login', component:LogInComponent},
-  {path:'register', component:RegisterComponent}
+  {path:'register', component:RegisterComponent},
+  {path:'forgotpassword', component:ForgotPasswordComponent},
+  {path:'resetpassword',component:ResetPasswordComponent}
 ];
 
 @NgModule({
