c805ce670f67a71bd1770851a6ddc7e881812c36
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-12 22:49:37 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,17 @@ const app = express();
 require('dotenv').config();
 
 // Now, you can access your environment variables like this:
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;;
 const databaseUrl = process.env.DATABASE_URL;
 const apiKey = process.env.API_KEY;
 
 // Use these variables in your application as needed.
-
-
+app.use(express.json());
+mongoose.connect(databaseUrl).then(()=>{
+    console.log('successfully connected with database')
+}).catch((err)=>{
+    console.error('error in connecting with database',err)
+})
 
 
 
