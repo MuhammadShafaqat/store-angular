7935ad4daaa6e7ca8de79139374f72d2160f921b
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-16 23:21:10 +0500

    update

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -16,6 +16,36 @@ router.get('/', async (req, res) => {
       return res.status(500).json({ success: false, message: 'Server Error' });
     }
   });
+// For a single user
+router.get('/:id', async (req, res) => {
+    try {
+      const user = await UserModel.findById(req.params.id).select('-password');
+      if (!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+      }
+      return res.status(200).json({ success: true, message: 'User retrieved successfully', user: user });
+    } catch (error) {
+      console.error('Error in retrieving user:', error);
+      return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+  });
+
+// Delete a user by ID
+router.delete('/:id', async (req, res) => {
+    try {
+      const user = await UserModel.findByIdAndDelete(req.params.id);
+      if (!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+      } 
+      return res.status(200).json({ success: true, message: 'User deleted successfully' });
+    } catch (error) {
+      console.error('Error in deleting user:', error);
+      return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+  });
+  
+  
+  
 
   module.exports = router
   
