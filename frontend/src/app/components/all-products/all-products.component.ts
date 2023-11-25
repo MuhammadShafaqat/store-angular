ce7ab57a4faf06c5d3bfc642f63b20db83392594
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-11-25 21:39:27 +0500

    update

--- frontend/src/app/components/all-products/all-products.component.ts
+++ frontend/src/app/components/all-products/all-products.component.ts
@@ -8,6 +8,7 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class AllProductsComponent implements OnInit {
   products: any[] = [];
+  filterProducts: any[] = [];
   error!: string;
 
 constructor(private post:PostService){}
@@ -15,6 +16,7 @@ constructor(private post:PostService){}
   ngOnInit(): void {
     this.post.getData().subscribe((data: any)=>{
       this.products = data
+      this.filterProducts = data
       this.products = this.products.filter(product => product.id <= 10)
       console.log(this.products)
 
@@ -24,4 +26,17 @@ constructor(private post:PostService){}
     }
     )
   }
+  allProducts(){
+  this.products = this.filterProducts.filter((item)=> item)
+  }
+  electronics(){
+ this.products = this.filterProducts.filter((item)=>item.category === 'electronics')
+}
+fashion(){
+  this.products = this.filterProducts.filter((item)=> item.category === "men's clothing" || item.category === "women's clothing")
+}
+jewelery(){
+  this.products = this.filterProducts.filter((item)=> item.category === 'jewelery')
+}
+
 }
